7c6a4954c057f5563071be7cf78496c6f9ffcbdb
EdgarsFeic96 <edgar1569@outlook.com>
2023-03-19 21:39:28 -0600

    moved Stack definitions to header file

--- stack/main.cpp
+++ stack/main.cpp
@@ -1,27 +1,27 @@
-#include <iostream>
 #include "stack.h"
+#include <iostream>
 
-int main(int argc, char const* argv[])
+int main(int argc, char const *argv[])
 {
-  int stack_size = 15;
-  Stack<int> my_stack(stack_size);
+	int stack_size = 15;
+	Stack<int> my_stack(stack_size);
 
-  std::cout << "Pushing values: ";
-  for (int i = 0; i < stack_size; i++)
-  {
-    std::cout << i << " ";
-    my_stack.push(i);
-  }
-  std::cout << std::endl;
+	std::cout << "Pushing values: ";
+	for (int i = 0; i < stack_size; i++)
+	{
+		std::cout << i << " ";
+		my_stack.push(i);
+	}
+	std::cout << std::endl;
 
-  std::cout << "Popping values: ";
-  for (int i = 0; i < stack_size; i++)
-  {
-    auto value = my_stack.pop();
-    if (value != nullptr)
-      std::cout << *value << " ";
-  }
-  std::cout << std::endl;
+	std::cout << "Popping values: ";
+	for (int i = 0; i < stack_size; i++)
+	{
+		auto value = my_stack.pop( );
+		if (value != nullptr)
+			std::cout << *value << " ";
+	}
+	std::cout << std::endl;
 
-  return 0;
+	return 0;
 }
--- stack/stack.h
+++ stack/stack.h
@@ -12,27 +12,57 @@
 template <class T>
 class Stack
 {
-private:
-  std::unique_ptr<T[]> _data;
-  int _top;
-  int _max_size;
-public:
-  Stack(int max_size);
-  /**
-   *@brief Push an value to the stack.
-   * @tparam T An value of the type defined in the stack
-   * @param value New value
-   */
-  void push(T value);
+  private:
+	std::unique_ptr<T[]> _data;
+	int _top;
+	int _max_size;
 
-  /**
-   *@brief Pops the last value in the stack
-   *
-   * @return T* Last value in the stack
-   */
-  T* pop();
+  public:
+	Stack(int max_size);
+	/**
+	 *@brief Push an value to the stack.
+	 * @tparam T An value of the type defined in the stack
+	 * @param value New value
+	 */
+	void push(T value);
+
+	/**
+	 *@brief Pops the last value in the stack
+	 *
+	 * @return T* Last value in the stack
+	 */
+	T *pop( );
 };
 
-#include "stack.cpp"
+template <class T>
+Stack<T>::Stack(int max_size)
+{
+	this->_max_size = max_size;
+	this->_data = std::make_unique<T[]>(this->_max_size * sizeof(T));
+	this->_top = -1;
+}
+
+template <class T>
+void Stack<T>::push(T value)
+{
+	if (this->_top >= this->_max_size - 1)
+		std::cerr << "Stack overflow\n";
+	else
+		this->_data[++this->_top] = value;
+}
+
+template <class T>
+T *Stack<T>::pop( )
+{
+	if (this->_top < 0)
+	{
+		std::cerr << "Empty stack\n";
+		return nullptr;
+	}
+	else
+	{
+		return &this->_data[this->_top--];
+	}
+}
 
 #endif
