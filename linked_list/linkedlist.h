e6b13be8b2671f5ade8dd067c792a2a87355d2f4
EdgarsFeic96 <edgar1569@outlook.com>
2023-04-06 16:18:54 -0600

    Added Linked List

--- linked_list/linkedlist.h
+++ linked_list/linkedlist.h
@@ -4,6 +4,7 @@
 #include "node.h"
 #include <iostream>
 #include <memory>
+#include <stdexcept>
 
 template <class T>
 class LinkedList
@@ -17,11 +18,10 @@ class LinkedList
 	void insert(T value);
 	void insert(T value, int index);
 	void remove(int index);
-	void size();
+	int size();
 	friend std::ostream &operator<<(std::ostream &os, const LinkedList<T> &list)
 	{
-		if (list._head == nullptr)
-			return os;
+		if (list._head == nullptr) return os;
 
 		Node<T> *position = list._head.get();
 
@@ -42,6 +42,7 @@ template <class T>
 LinkedList<T>::LinkedList()
 {
 	_head = nullptr;
+	_size = 0;
 }
 
 template <class T>
@@ -58,6 +59,76 @@ void LinkedList<T>::insert(T value)
 		position = position->get_next().get();
 
 	position->set_next(std::make_shared<Node<T>>(Node<T>(value)));
+	_size++;
+}
+
+template <class T>
+void LinkedList<T>::insert(T value, int index)
+{
+	if (index >= _size) throw std::out_of_range("Index out of range!");
+
+	std::shared_ptr<Node<T>> new_node =
+	    std::make_shared<Node<T>>(Node<T>(value));
+
+	if (_head == nullptr) { _head = new_node; }
+	else if (index == 0)
+	{
+		new_node.get()->set_next(_head);
+		_head = new_node;
+	}
+	else if (index == _size) { this->insert(value); }
+	else
+	{
+
+		int pos = 0;
+		Node<T> *pos_node = _head.get();
+		while (pos_node->get_next() != nullptr && index > ++pos)
+		{
+			pos_node = pos_node->get_next().get();
+		}
+
+		new_node->set_next(pos_node->get_next());
+		pos_node->set_next(new_node);
+	}
+
+	_size++;
+}
+
+template <class T>
+void LinkedList<T>::remove(int index)
+{
+	if (index > _size) std::out_of_range("Index out of range!");
+	if (index == 0)
+	{
+		std::shared_ptr<Node<T>> old_node = _head;
+		_head = old_node.get()->get_next();
+		old_node.get()->set_next(nullptr);
+	}
+	else if (index == _size)
+	{
+		Node<T> *position = _head.get();
+		while (position->get_next()->get_next() != nullptr)
+			position = position->get_next().get();
+		position->set_next(nullptr);
+	}
+	else
+	{
+		int pos = 0;
+		Node<T> *pos_node = _head.get();
+		while (pos_node->get_next() != nullptr && index > ++pos)
+			pos_node = pos_node->get_next().get();
+
+		std::shared_ptr<Node<T>> old_node = pos_node->get_next();
+		pos_node->set_next(old_node.get()->get_next());
+	}
+
+	_size--;
+}
+
+template <class T>
+inline int LinkedList<T>::size()
+{
+	return _size;
 }
 
 #endif
--- linked_list/main.cpp
+++ linked_list/main.cpp
@@ -2,12 +2,21 @@
 #include <iostream>
 #include <ostream>
 
-
 int main()
 {
 	LinkedList<int> my_list;
-	my_list.insert(15);
-	my_list.insert(16);
-	my_list.insert(17);
-	std::cout << my_list << std::endl;
+
+	for (int i = 15; i <= 25; i++)
+		my_list.insert(i);
+
+	my_list.remove(0);              // 15 in list, remove head
+	my_list.remove(my_list.size()); // 25 in list, remove end
+	my_list.remove(3);              // 19 in list, remove index
+
+	my_list.insert(15, 0); // insert to head
+	my_list.insert(25);    // insert to end
+	my_list.insert(19, 4); // insert to index
+
+	std::cout << my_list << "\n";
+	std::cout << "Size: " << my_list.size() << "\n";
 }
